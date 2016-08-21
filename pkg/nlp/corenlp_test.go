df6495b2f0157121ccf65ae1f939e267001a27ac
Konrad Strack <konrad@strack.pl>
2016-08-21 12:14:13 +0100

    Add CoreNLP parse tests

--- pkg/nlp/corenlp_test.go
+++ pkg/nlp/corenlp_test.go
@@ -1,6 +1,12 @@
 package nlp
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
 
 func TestBuildURL(t *testing.T) {
 	config := &CoreNLPConfig{
@@ -13,4 +19,82 @@ func TestBuildURL(t *testing.T) {
 	if actualURL != expectedURL {
 		t.Errorf("Expected %s, got %s", expectedURL, actualURL)
 	}
+}
+
+func TestParse(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(coreNLPHandler))
+	defer s.Close()
+
+	config := &CoreNLPConfig{
+		BaseURL: s.URL,
+	}
+
+	client, _ := NewCoreNLPClient(config)
+	text, _ := client.Parse("I like pie")
+	if actual, expected := len(text.Sentences), 1; actual != expected {
+		t.Errorf("Expected %d sentences, got %d", expected, actual)
+	}
+
+	tokens := text.Sentences[0].Tokens
+	actualPosTags := make([]string, 3)
+	for i, t := range tokens {
+		actualPosTags[i] = t.POS
+	}
+
+	expectedPosTags := []string{"PRP", "VBP", "NN"}
+	if !reflect.DeepEqual(actualPosTags, expectedPosTags) {
+		t.Errorf("Expected %v, got %v", expectedPosTags, actualPosTags)
+	}
+}
+
+var parserResponse = `
+{
+  "sentences": [
+    {
+      "index": 0,
+      "tokens": [
+        {
+          "index": 1,
+          "word": "I",
+          "originalText": "I",
+          "lemma": "I",
+          "characterOffsetBegin": 0,
+          "characterOffsetEnd": 1,
+          "pos": "PRP",
+          "ner": "O",
+          "before": "",
+          "after": " "
+        },
+        {
+          "index": 2,
+          "word": "like",
+          "originalText": "like",
+          "lemma": "like",
+          "characterOffsetBegin": 2,
+          "characterOffsetEnd": 6,
+          "pos": "VBP",
+          "ner": "O",
+          "before": " ",
+          "after": " "
+        },
+        {
+          "index": 3,
+          "word": "pie",
+          "originalText": "pie",
+          "lemma": "pie",
+          "characterOffsetBegin": 7,
+          "characterOffsetEnd": 10,
+          "pos": "NN",
+          "ner": "O",
+          "before": " ",
+          "after": ""
+        }
+      ]
+    }
+  ]
+}
+`
+
+func coreNLPHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, parserResponse)
 }
